c739ea70afb2aee01b06922a18f5261b609bc583
Eric Souza <ericsouza0801@gmail.com>
2023-08-07 10:15:55 -0300

    configura logging gelf

--- src/main/java/com/ericsouza/GreetingResource.java
+++ src/main/java/com/ericsouza/GreetingResource.java
@@ -1,5 +1,7 @@
 package com.ericsouza;
 
+import org.jboss.logging.Logger;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +10,14 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("Log INFO level");
+        LOG.warn("Log WARN level");
+        LOG.error("Log ERROR level");
         return "Hello from RESTEasy Reactive";
     }
 }
